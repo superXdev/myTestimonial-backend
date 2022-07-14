339bc3470f2349a68256b0ed6751e9033505328c
Fikri <fikrizhanking@gmail.com>
2022-07-14 14:03:14 +0700

    Add migrate helper

--- migrate-force.js
+++ migrate-force.js
@@ -1,11 +1,13 @@
 import db from "./config/database.js";
 
-try {
-    await db.authenticate();
-    db.sync({ force: true });
-    await Setting.create({ 
-        serverStarted: Date.now() 
-    })
-} catch (error) {
-    throw Error(error.message);
-}
+(async function () {
+    try {
+        await db.authenticate();
+        db.sync({ force: true });
+        await Setting.create({ 
+            serverStarted: Date.now() 
+        });
+    } catch (error) {
+        throw Error(error.message);
+    }
+})
